e8c540f9226c4bf80c35ab55009570fa9c211836
nojino <nojinonoue@yahoo.co.jp>
2023-05-22 22:57:22 +0900

    test

--- src/app/Providers/FortifyServiceProvider.php
+++ src/app/Providers/FortifyServiceProvider.php
@@ -50,10 +50,6 @@ class FortifyServiceProvider extends ServiceProvider
         return view('auth.reset-password', ['request' => $request]);
     });
 
-    Fortify::resetPasswordView(function ($request) {
-        return view('auth.reset-password', ['request' => $request]);
-    });
-
     Fortify::resetUserPasswordsUsing(ResetUserPassword::class);
 
     }
